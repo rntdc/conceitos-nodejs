30809a0ac8b95d7bc57d3eb4f58c04579b728e91
Renato <rntdek@gmail.com>
2020-10-21 21:15:15 -0300

    update e ajuste like

--- src/app.js
+++ src/app.js
@@ -29,7 +29,24 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title, url, techs} = request.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." })
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
